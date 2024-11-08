9cbae0ef570653159060bfd47f6ed02e2013097d
Davide Mancini <davide.mancini10@studio.unibo.it>
2024-11-08 15:37:51 +0100

    Concluded ex 71

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,22 +1,23 @@
 package it.unibo.inner.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 import it.unibo.inner.api.IterableWithPolicy;
 import it.unibo.inner.api.Predicate;
 
-import java.util.Iterator;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
-    private final IteratorWithoutPolicy<T> iterator;
-    private final Predicate<T> predicate;
-    private class IteratorWithoutPolicy<T> implements Iterator<T> {
+    private final IteratorWithPolicy iterator;
+    private Predicate<T> predicate;
+    
+    private class IteratorWithPolicy implements Iterator<T> {
 
         private final ArrayList<T> arrayToIterate;
         private int current;
 
-        public IteratorWithoutPolicy(final T[] array) {
+        public IteratorWithPolicy(final T[] array) {
             this.arrayToIterate = new ArrayList<T>(Arrays.asList(array));
             this.current = 0;
         }
@@ -28,26 +29,48 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
         @Override
         public boolean hasNext() {
-            return this.current < this.arrayToIterate.size();
+            while (
+                this.current < this.arrayToIterate.size()
+                && !predicate.test(this.arrayToIterate.get(this.current))
+            ) {
+                this.current++;
+            }
+
+            return this.current < this.arrayToIterate.size() 
+                && predicate.test(this.arrayToIterate.get(this.current));
+        }
+
+        @Override
+        public String toString() {
+            return arrayToIterate.toString();
         }
     };
 
     public IterableWithPolicyImpl(final T[] array) {
-        this(array, null);
+        this(array, new Predicate<>() {
+            public boolean test(final T elem) {
+                return true;
+            }
+        });
     }
 
     public IterableWithPolicyImpl(final T[] array, final Predicate<T> predicate) {
-        this.iterator = new IteratorWithoutPolicy<T>(array);
+        this.iterator = new IteratorWithPolicy(array);
         this.predicate = predicate;
     }
 
     @Override
     public void setIterationPolicy(final Predicate<T> filter) {
-
+        this.predicate = filter;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return iterator;
+        return this.iterator;
+    }
+    
+    @Override
+    public String toString() {
+        return this.iterator.arrayToIterate.toString();
     }
 }
